d1ccfff7f2dbb536b423e3317627752d94456d7b
vietthang09 <vthcvn@gmail.com>
2023-02-16 21:42:12 +0700

    feat: design responsive for navigation bar, footer

--- src/App.js
+++ src/App.js
@@ -84,26 +84,30 @@ function App() {
     }
   };
 
+  function checkLogin() {
+    const token = cookie.load("token");
+    if (token == null || token == undefined) return false;
+    else return true;
+  }
+
   const UserLogin = () => {
     const token = cookie.load("token");
     if (token === null || token === undefined) {
       return (
-        <div className="navbar-nav ml-auto py-0 d-none d-lg-block">
-          <Link to="/login" className="btn px-0 ml-3">
-            <button
-              type="button"
-              className="btn btn-sm"
-              style={{
-                background: "#17a2b8",
-                color: "#fff",
-                fontSize: "16px",
-                marginTop: "9px",
-              }}
-            >
-              Login
-            </button>
-          </Link>
-        </div>
+        <Link to="/login" className="btn">
+          <button
+            type="button"
+            className="btn btn-sm"
+            style={{
+              background: "#17a2b8",
+              color: "#fff",
+              fontSize: "16px",
+              marginTop: "9px",
+            }}
+          >
+            Login
+          </button>
+        </Link>
       );
     } else {
       const status = getUser(token);
@@ -120,14 +124,23 @@ function App() {
             <Link to="/wishlist" className="btn px-0">
               <i
                 className="fas fa-heart text-primary"
-                style={{ paddingBottom: "2px", marginTop: "9px",fontSize: "30px" }}
+                style={{
+                  paddingBottom: "2px",
+                  marginTop: "9px",
+                  fontSize: "30px",
+                }}
               ></i>
             </Link>
             <Link to="/shoppingcart" className="btn px-0 ml-3">
-              <i className="fas fa-shopping-cart text-primary" style={{paddingBottom: "2px", marginTop: "9px" ,fontSize: "30px"}}></i>
-              <span
-                className="badge text-secondary border border-secondary rounded-circle"
-              >
+              <i
+                className="fas fa-shopping-cart text-primary"
+                style={{
+                  paddingBottom: "2px",
+                  marginTop: "9px",
+                  fontSize: "30px",
+                }}
+              ></i>
+              <span className="badge text-secondary border border-secondary rounded-circle">
                 {cart ? cart.length : 0}
               </span>
             </Link>
@@ -167,111 +180,212 @@ function App() {
   };
 
   const Header = () => (
-    <div className="Header-nav fixed-top">
-        <ToastContainer
-          position="top-right"
-          autoClose={2000}
-          hideProgressBar={false}
-          newestOnTop={false}
-          closeOnClick
-          rtl={false}
-          pauseOnFocusLoss
-          draggable
-          pauseOnHover
-          theme="light"
-        />
-      <div className="container-fluid bg-dark mb-30">
-        <div className="row px-xl-5">
-          <div className="col-lg-1 d-none d-lg-block" style={{ paddingBottom: "2px", marginTop: "15px" }}>
-            <Link to="" className="text-decoration-none">
-              <span className="h3 text-uppercase text-info bg-dark px-2">
-                SKA
-              </span>
-              <span className="h3 text-uppercase text-white bg-info px-2 ml-n1">
-                BUY
-              </span>
+    <div>
+      <div className="d-lg-none">
+        <div className="d-flex">
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Search for products"
+            onKeyDown={(e) => onKeyDownHandler(e)}
+            onChange={(e) => setKeyword(e.target.value)}
+          />
+          <button
+            className="input-group-text text-primary bg-info"
+            onClick={() => onSearchHandler()}
+          >
+            <i className="fa fa-search" style={{ color: "#fff" }}></i>
+          </button>
+        </div>
+
+        <div className="fixed-bottom px-3 py-2 bg-white">
+          {!checkLogin ? (
+            <div className="d-flex justify-content-between">
+              <Link to="/">
+                <i
+                  class="fa fa-home text-primary"
+                  style={{
+                    fontSize: "24px",
+                  }}
+                  aria-hidden="true"
+                ></i>
+              </Link>
+              <Link to="/shoppingcart">
+                <i
+                  className="fas fa-shopping-cart"
+                  style={{
+                    fontSize: "24px",
+                  }}
+                  aria-hidden="true"
+                ></i>
+              </Link>
+              <Link to="/wishlist">
+                <i
+                  className="fas fa-heart"
+                  style={{
+                    fontSize: "24px",
+                  }}
+                  aria-hidden="true"
+                ></i>
+              </Link>
+              <Link to="/profile">
+                <i
+                  class="fa fa-user"
+                  style={{
+                    fontSize: "24px",
+                  }}
+                  aria-hidden="true"
+                ></i>
+              </Link>
+            </div>
+          ) : (
+            <Link
+              to="/login"
+              className="d-flex justify-content-center text-info"
+            >
+              Login
             </Link>
+          )}
+        </div>
+      </div>
+      <div className="d-none d-lg-block">
+        <div className="Header-nav">
+          <div className="container-fluid">
+            <ToastContainer
+              position="top-right"
+              autoClose={2000}
+              hideProgressBar={false}
+              newestOnTop={false}
+              closeOnClick
+              rtl={false}
+              pauseOnFocusLoss
+              draggable
+              pauseOnHover
+              theme="light"
+            />
+            <div className="row justify-content-between align-items-center bg-light py-3 px-xl-5 d-none d-lg-flex">
+              <div className="col-lg-4">
+                <Link to="" className="text-decoration-none">
+                  <span className="h1 text-uppercase text-info bg-dark px-2">
+                    HLE
+                  </span>
+                  <span className="h1 text-uppercase text-white bg-info px-2 ml-n1">
+                    Shop
+                  </span>
+                </Link>
+              </div>
+              <div className="col-lg-4 col-6 text-right">
+                <p className="m-0">Customer Service</p>
+                <h5 className="m-0">+012 345 6789</h5>
+              </div>
+            </div>
           </div>
-          <div className="col-lg-2 d-none d-lg-block">
-            <a
-              className="btn d-flex align-items-center justify-content-between bg-info w-100"
-              data-toggle="collapse"
-              id="menubar"
-              style={{ height: "65px", padding: "0 30px" }}
-            >
-              <h6 className="text-white m-0">
-                <i className="fa fa-bars mr-2"></i>Categories
-              </h6>
-              <i className="fa fa-angle-down text-dark"></i>
-            </a>
-            <nav
-              className="collapse position-absolute navbar navbar-vertical navbar-light align-items-start p-0 bg-light"
-              id="navbar-vertical"
-              style={{ width: "calc(100% - 30px)", zIndex: "999" }}
-            >
-              <div className="navbar-nav w-100">
-                {categoryList.map((val, key) => (
-                  <div className="pb-1 nav-link nav-item menuhover" key={key}>
-                    <Link
-                      className="text-decoration-none"
-                      to={val.category_slug}
-                      state={{ category_id: val.category_id }}
-                    >
-                      <div className="d-flex align-items-center">
-                        <img
-                          className="img-fluid"
-                          src={val.category_image}
-                          alt=""
-                          style={{
-                            padding: "10px",
-                            height: "50px",
-                            alignItems: "center",
-                          }}
-                        />
-                        <div className="flex-fill pl-3">
-                          <h6 className="">{val.category_name}</h6>
+          <div className="container-fluid bg-dark mb-30">
+            <div className="row px-xl-5">
+              <div className="col-lg-3 d-none d-lg-block">
+                <a
+                  className="btn d-flex align-items-center justify-content-between bg-info w-100"
+                  data-toggle="collapse"
+                  id="menubar"
+                  style={{ height: "65px", padding: "0 30px" }}
+                >
+                  <h6 className="text-white m-0">
+                    <i className="fa fa-bars mr-2"></i>Categories
+                  </h6>
+                  <i className="fa fa-angle-down text-dark"></i>
+                </a>
+                <nav
+                  className="collapse position-absolute navbar navbar-vertical navbar-light align-items-start p-0 bg-light"
+                  id="navbar-vertical"
+                  style={{ width: "calc(100% - 30px)", zIndex: "999" }}
+                >
+                  <div className="navbar-nav w-100">
+                    {categoryList.map((val, key) => (
+                      <div
+                        className="pb-1 nav-link nav-item menuhover"
+                        key={key}
+                      >
+                        <Link
+                          className="text-decoration-none"
+                          to={val.category_slug}
+                          state={{ category_id: val.category_id }}
+                        >
+                          <div className="d-flex align-items-center">
+                            <img
+                              className="img-fluid"
+                              src={val.category_image}
+                              alt=""
+                              style={{
+                                padding: "10px",
+                                height: "50px",
+                                alignItems: "center",
+                              }}
+                            />
+                            <div className="flex-fill pl-3">
+                              <h6 className="">{val.category_name}</h6>
+                            </div>
+                          </div>
+                        </Link>
+                      </div>
+                    ))}
+                  </div>
+                </nav>
+              </div>
+              <div className="col-lg-9">
+                <nav className="navbar navbar-expand-lg bg-dark navbar-dark py-3 py-lg-0 px-0">
+                  <Link
+                    to="/"
+                    className="text-decoration-none d-block d-lg-none"
+                  >
+                    <span className="h1 text-uppercase text-dark bg-light px-2">
+                      Multi
+                    </span>
+                    <span className="h1 text-uppercase text-light bg-primary px-2 ml-n1">
+                      Shop
+                    </span>
+                  </Link>
+                  <button
+                    type="button"
+                    className="navbar-toggler"
+                    data-toggle="collapse"
+                    data-target="#navbarCollapse"
+                  >
+                    <span className="navbar-toggler-icon"></span>
+                  </button>
+                  <div
+                    className="collapse navbar-collapse justify-content-between"
+                    id="navbarCollapse"
+                  >
+                    <div className="navbar-nav mr-auto py-0">
+                      <div className="searchmb" style={{ height: "20px" }}>
+                        <div className="input-group nav-item">
+                          <input
+                            type="text"
+                            className="form-control"
+                            placeholder="Search for products"
+                            onKeyDown={(e) => onKeyDownHandler(e)}
+                            onChange={(e) => setKeyword(e.target.value)}
+                          />
+                          <div className="input-group-append">
+                            <button
+                              className="input-group-text text-primary bg-info"
+                              onClick={() => onSearchHandler()}
+                            >
+                              <i
+                                className="fa fa-search"
+                                style={{ color: "#fff" }}
+                              ></i>
+                            </button>
+                          </div>
                         </div>
                       </div>
-                    </Link>
-                  </div>
-                ))}
-              </div>
-            </nav>
-          </div>
-          <div className="col-lg-9">
-            <nav className="navbar navbar-expand-lg bg-dark navbar-dark py-3 py-lg-0 px-0">
-              <div
-                className="collapse navbar-collapse justify-content-between"
-                id="navbarCollapse"
-              >
-                <div className="navbar-nav mr-auto py-0">
-                  <div className="searchmb" style={{ height: "20px" }}>
-                    <div className="input-group nav-item">
-                      <input
-                        type="text"
-                        className="form-control"
-                        placeholder="Search for products"
-                        onKeyDown={(e) => onKeyDownHandler(e)}
-                        onChange={(e) => setKeyword(e.target.value)}
-                      />
-                      <div className="input-group-append">
-                        <button
-                          className="input-group-text text-primary bg-info"
-                          onClick={() => onSearchHandler()}
-                        >
-                          <i
-                            className="fa fa-search"
-                            style={{ color: "#fff" }}
-                          ></i>
-                        </button>
-                      </div>
                     </div>
-                  </div>
-                </div>
 
-                {UserLogin()}
+                    {UserLogin()}
+                  </div>
+                </nav>
               </div>
-            </nav>
+            </div>
           </div>
         </div>
       </div>
@@ -281,35 +395,34 @@ function App() {
   const body = () => (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/vnpay-return" element={<Home />} />
-      <Route path="/:category_slug" element={<ProductByCategory />} />
-      <Route path="/search/:keyword" element={<SearchResult />} />
-      <Route path="/home" element={<Home />} />
-      <Route path="/wishlist" element={<Wishlist />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
-      <Route path="/detailshop/:slug" element={<DetailShop />} />
+      <Route path="/home" element={<Home />} />
       <Route
         path="/shoppingcart"
         element={<ShoppingCart setUpdateCart={setUpdateCart} />}
       />
+      <Route path="/wishlist" element={<Wishlist />} />
+      <Route path="/profile" element={<Profile />} />
+      <Route path="/search/:keyword" element={<SearchResult />} />
+      <Route path="/:category_slug" element={<ProductByCategory />} />
+      <Route path="/detailshop/:slug" element={<DetailShop />} />
       <Route
         path="/checkout"
         element={<Checkout setUpdateCart={setUpdateCart} />}
-      /><Route
+      />
+      <Route
         path="/process-checkout"
         element={<ProcessCheckout setUpdateCart={setUpdateCart} />}
       />
-      <Route path="/profile" element={<Profile />} />
       <Route path="/account-verification" element={<AccountVerification />} />
-
       <Route path="/loginadmin" element={<LoginAdmin />} />
       <Route path="/admin/*" element={<Admin />} />
     </Routes>
   );
 
   const footer = () => (
-    <div className="footer container-fluid bg-dark text-secondary mt-5 pt-5">
+    <div className="d-none d-lg-block footer container-fluid bg-dark text-secondary mt-5 pt-5">
       <div className="row px-xl-5 pt-5">
         <div className="col-lg-4 col-md-12 mb-5 pr-3 pr-xl-5">
           <h5 className="text-secondary text-uppercase mb-4">HLE E-commere</h5>
@@ -436,18 +549,12 @@ function App() {
     </div>
   );
 
-  const initialOptions = {
-    "client-id": "test",
-    currency: "USD",
-    intent: "capture",
-    "data-client-token": "abc123xyz==",
-  };
   return (
     <>
       {Header()}
       {body()}
       {footer()}
-      <div className="chatbot">{ChatBotPage()}</div>
+      {/* <div className="chatbot">{ChatBotPage()}</div> */}
     </>
   );
 }
